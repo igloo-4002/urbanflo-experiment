37711fb18943c8cfa880c3e21ed0b98d03f36735
elysiacasandra <egia0002@student.monash.edu>
2023-08-14 11:26:50 +1000

    Added the con file download. If an edges to is the same as another edges from- its a connection. Will need to be refined in future

--- src/components/FloatingPlayPause.tsx
+++ src/components/FloatingPlayPause.tsx
@@ -4,10 +4,13 @@ const FloatingPlayPause = ({ nodes, edges }) => {
   const handleDownload = () => {
     const xmlContentNodes = generateXmlNodes(nodes);
     const xmlContentEdges = generateXmlEdges(edges);
+    const xmlContentConnections = generateXmlConnections(edges);
     const blobNodes = new Blob([xmlContentNodes], { type: "application/xml" });
     const blobEdges = new Blob([xmlContentEdges], { type: "application/xml" });
+    const blobConnections = new Blob([xmlContentConnections], { type: "application/xml" });
     const urlNodes = URL.createObjectURL(blobNodes);
     const urlEdges = URL.createObjectURL(blobEdges);
+    const urlConnections = URL.createObjectURL(blobConnections);
     const a = document.createElement("a");
     a.href = urlNodes;
     a.download = "t.nod.xml";
@@ -18,6 +21,11 @@ const FloatingPlayPause = ({ nodes, edges }) => {
     b.download = "t.edg.xml";
     b.click();
     URL.revokeObjectURL(urlEdges);
+    const c = document.createElement("a");
+    c.href = urlConnections;
+    c.download = "t.con.xml";
+    c.click();
+    URL.revokeObjectURL(urlConnections);
   };
 
   const generateXmlNodes = (nodes) => {
@@ -43,6 +51,40 @@ const FloatingPlayPause = ({ nodes, edges }) => {
       <edges>
         ${xmlEdges.join("\n")}
       </edges>
+    `;
+
+    return xml;
+  };
+
+  const generateXmlConnections = (edges) => {
+    // You can create your own XML structure here based on the nodes and edges data
+    // For simplicity, I'll use a basic example
+
+    const connections = [];
+
+    for (let i = 0; i < edges.length; i++) {
+      for (let j = 0; j < edges.length; j++) {
+        if (edges[i].to === edges[j].from) {
+          const from = edges[i].id;
+          const to = edges[j].id;
+          const fromLane = 0;
+          const toLane = 0;
+    
+          connections.push({ from, to, fromLane, toLane });
+        }
+      }
+    }
+    
+    const xmlConnections = connections.map(
+      (connection) =>
+        `<connection from="${connection.from}" to="${connection.to}" fromLane="${connection.fromLane}" toLane="${connection.toLane}" />`
+    );
+
+
+    const xml = `
+      <connections>
+        ${xmlConnections.join("\n")}
+      </connections>
     `;
 
     return xml;
